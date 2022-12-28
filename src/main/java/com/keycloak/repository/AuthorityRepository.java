ca4440c8ad4f7ca9fbb44dfa097d471a93aba60e
Ubuntu <ubuntu@ip-172-31-42-89.ap-south-1.compute.internal>
2022-12-28 13:59:25 +0530

    Initial version of KeycloakIntegration generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/keycloak/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.keycloak.repository;
+
+import com.keycloak.domain.Authority;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {}
